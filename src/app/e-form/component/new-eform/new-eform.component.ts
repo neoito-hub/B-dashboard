4cdd735655dedf7f444f82513f1b6d12655d887e
daliyajohn <daliyajohn75@gmail.com>
2018-11-03 22:06:51 +0530

    Image upload UI

--- src/app/e-form/component/new-eform/new-eform.component.ts
+++ src/app/e-form/component/new-eform/new-eform.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const $: any;
 @Component({
   selector: 'app-new-eform',
   templateUrl: './new-eform.component.html',
   styleUrls: ['./new-eform.component.css']
 })
 export class NewEformComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {}
 }
