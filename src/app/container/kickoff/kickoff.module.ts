3a15b2f1390321072af0bbcbe6dbf0d550da3d95
Sneha P P <sneha.pp@neoito.com>
2018-11-03 12:51:45 +0530

    kickoff folder added in component section

--- src/app/container/kickoff/kickoff.module.ts
+++ src/app/container/kickoff/kickoff.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KickoffComponent } from './kickoff.component';
 import { RouterModule, Routes } from '@angular/router';
-import { ClientsComponent } from '../../component/clients/clients.component';
+import { ClientsComponent } from '../../component/kickoff/clients/clients.component';
 
 const routes: Routes = [
   { path: '', component: KickoffComponent },
