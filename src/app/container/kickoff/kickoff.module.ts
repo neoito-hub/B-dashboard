3ccb05676c646778b33875a43c0a035f83c5fbce
Abin Punnoose <abin.punnoose@neoito.com>
2018-11-03 13:07:32 +0530

    resources, roles, service component added for kickoff

--- src/app/container/kickoff/kickoff.module.ts
+++ src/app/container/kickoff/kickoff.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 import { KickoffComponent } from './kickoff.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientsComponent } from '../../component/kickoff/clients/clients.component';
+import { RolesComponent } from '../../component/kickoff/roles/roles.component';
+import { ServicesComponent } from '../../component/kickoff/services/services.component';
+import { ResourcesComponent } from '../../component/kickoff/resources/resources.component';
 
 const routes: Routes = [
-  { path: '', component: KickoffComponent },
-  { path: 'clients', component: ClientsComponent }
+  { path: '', redirectTo: 'clients', pathMatch: 'full' },
+  { path: 'clients', component: ClientsComponent },
+  { path: 'roles', component: RolesComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'resources', component: ResourcesComponent }
 ];
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [KickoffComponent, ClientsComponent]
+  declarations: [
+    KickoffComponent,
+    ClientsComponent,
+    RolesComponent,
+    ServicesComponent,
+    ResourcesComponent
+  ]
 })
 export class KickoffModule {}
