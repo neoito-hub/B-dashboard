5e834b07f9ff674892543c85f1e2881647ac7191
Sneha P P <sneha.pp@neoito.com>
2018-11-03 12:49:35 +0530

    clients component added

--- src/app/container/kickoff/kickoff.module.ts
+++ src/app/container/kickoff/kickoff.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KickoffComponent } from './kickoff.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsComponent } from '../../component/clients/clients.component';
 
-const routes: Routes = [{ path: '', component: KickoffComponent }];
+const routes: Routes = [
+  { path: '', component: KickoffComponent },
+  { path: 'clients', component: ClientsComponent }
+];
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [KickoffComponent]
+  declarations: [KickoffComponent, ClientsComponent]
 })
 export class KickoffModule {}
