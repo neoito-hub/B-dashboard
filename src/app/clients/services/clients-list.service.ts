3bc15b5fb7ce5e60b93f337adace14421dbdca1d
Mubashir-Mohamed <mubashir.mohamed@neoito.com>
2018-11-10 11:31:48 +0530

    service

--- src/app/clients/services/clients-list.service.ts
+++ src/app/clients/services/clients-list.service.ts
@@ -4,10 +4,22 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ClientsListService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
   clientList = [
     { cname: 'rakesh', pname: 'propertok', email: 'e@d.c', skypeid: 'dd@liv' },
     { cname: 'sura', pname: 'milla', email: 'vv@mk.c', skypeid: 'aaa@liv' }
+  ];
+  clientDocuments = [
+    {
+      cname: 'rakesh', documents: [
+        { docId: '1', docName: 'BRD', docLink: '' }, { docId: '2', docName: 'FRD', docLink: '' }
+      ]
+    },
+    {
+      cname: 'sura', documents: [
+        { docId: '1', docName: 'BRD', docLink: '' }, { docId: '2', docName: 'FRD', docLink: '' }
+      ]
+    }
   ];
   getClientDetails(): Observable<any> {
     return Observable.create(observer => {
@@ -22,5 +34,12 @@ export class ClientsListService {
     //     return new ErrorObservable(res.error.errors[0]);
     //   })
     // );
+  }
+  getClientDocuments(name): Observable<any> {
+    let documents = this.clientDocuments.filter(element => { element.cname == name });
+    return Observable.create(observer => {
+      observer.next(documents);
+      observer.complete();
+    })
   }
 }
